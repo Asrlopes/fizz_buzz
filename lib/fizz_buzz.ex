9a441d7dd04847ac68e90f7ae19589386f669a02
Amoresio Souza <amoresio.souza@unemat.br>
2020-10-26 10:16:40 -0400

    Convertendo a string em uma Lista

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -5,6 +5,11 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
-  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def handle_file_read({:error, reason}), do: "Error while reading the file: #{reason}"
 end
